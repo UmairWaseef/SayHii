37f6ae4316378da6e5ec6046ae9ae68845eb2893
UmairWaseef <117443651+UmairWaseef@users.noreply.github.com>
2023-11-19 21:21:42 +0530

    first commit

---
+++ backend/src/main/java/com/individual/sayHi/SayHiApplication.java
@@ -0,0 +1,13 @@
+package com.individual.sayHi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayHiApplication.class, args);
+	}
+
+}
